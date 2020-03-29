32444b144f3ad4235d09eeada8a9eeb38a8052fd
Samuel Ramos <samuelsantos7@yahoo.com.br>
2020-03-28 23:48:24 -0300

    Configurações Iniciais

---
+++ backend/src/app.js
@@ -0,0 +1,10 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+module.exports = app;
