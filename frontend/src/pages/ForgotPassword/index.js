14ed2adebd1a7d00741dedea36c4180d616a3750
Samuel Ramos <samuelsantos7@yahoo.com.br>
2020-03-31 17:19:58 -0300

    Pagina de 'Esqueceu a senha' - Estilização

--- frontend/src/pages/ForgotPassword/index.js
+++ frontend/src/pages/ForgotPassword/index.js
@@ -1,11 +1,44 @@
-import React from 'react';
+import React from "react";
 
-// import { Container } from './styles';
+import {
+  Page,
+  Container,
+  Header,
+  Logo,
+  LinkBack,
+  Title,
+  Description,
+  Form,
+  ButtonSubmit
+} from "./styles";
+
+import Input from "./../../components/Input";
+import { FiMail, FiUser, FiArrowLeft } from "react-icons/fi";
+
+import logo from "./../../assets/logo.svg";
 
 export default function ForgotPassword() {
   return (
-    <div>
-      <h1>Forgot Password</h1>
-    </div>
+    <Page>
+      <Header>
+        <Logo src={logo} />
+        <LinkBack>
+          <FiArrowLeft style={{ marginRight: 20 }} size={20} />
+          Voltar ao login
+        </LinkBack>
+      </Header>
+
+      <Container>
+        <Title>Recuperar a Senha</Title>
+        <Description>Nos informe o seu nome de usuário e email.</Description>
+
+        <Form>
+          <Input placeholder="Usuário" icon={FiUser} />
+          <Input placeholder="E-mail" icon={FiMail} />
+
+          <ButtonSubmit>Lembrar-me</ButtonSubmit>
+        </Form>
+      </Container>
+    </Page>
   );
 }
